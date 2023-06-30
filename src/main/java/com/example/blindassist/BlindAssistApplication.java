04595a56430655ac186b56506d5dbf4ee69f6a98
m.mekhmonkuziev <@m.mekhmonkuziev>
2023-06-30 21:42:18 +0500

    Initial commit

---
+++ src/main/java/com/example/blindassist/BlindAssistApplication.java
@@ -0,0 +1,13 @@
+package com.example.blindassist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlindAssistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlindAssistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/blindassist/BlindAssistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blindassist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlindAssistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
