09ad8d7a64819cf4660a4cdb67c629b951ec5fe4
m.mekhmonkuziev <@m.mekhmonkuziev>
2023-06-30 21:44:06 +0500

    initial commit

--- src/main/java/com/example/blindassist/BlindAssistApplication.java
+++ src/main/java/com/example/blindassist/BlindAssistApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BlindAssistApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BlindAssistApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BlindAssistApplication.class, args);
+    }
 
 }
